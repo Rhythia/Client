c849f931fa2f8d30791e75075711acbb3910e05b
nyuwaea <luniwae@gmail.com>
2026-04-17 14:31:35 -0400

    space scripts cleanup

--- scripts/spaces/Grid.cs
+++ scripts/spaces/Grid.cs
@@ -1,27 +1,25 @@
-using System;
 using Godot;
 
-namespace Spaces
+namespace Spaces;
+
+public partial class Grid : BaseSpace
 {
-    public partial class Grid : BaseSpace
+    private StandardMaterial3D tileMaterial;
+    private WorldEnvironment environment;
+
+    public override void _Ready()
     {
-        private StandardMaterial3D tileMaterial;
-        private WorldEnvironment environment;
+        base._Ready();
 
-        public override void _Ready()
-        {
-            base._Ready();
+        tileMaterial = (GetNode<MeshInstance3D>("Top").Mesh as PlaneMesh).Material as StandardMaterial3D;
+        environment = GetNode<WorldEnvironment>("WorldEnvironment");
+    }
 
-            tileMaterial = (GetNode<MeshInstance3D>("Top").Mesh as PlaneMesh).Material as StandardMaterial3D;
-            environment = GetNode<WorldEnvironment>("WorldEnvironment");
-        }
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
 
-        public override void _Process(double delta)
-        {
-            base._Process(delta);
-
-            tileMaterial.AlbedoColor = NoteHitColor;
-            tileMaterial.Uv1Offset += Vector3.Up * (float)delta * 3;
-        }
+        tileMaterial.AlbedoColor = NoteHitColor;
+        tileMaterial.Uv1Offset += Vector3.Up * (float)delta * 3;
     }
 }
