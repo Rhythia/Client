10872884525deb75caf6432a4d74c7ec89fa1a5f
xwalfiee <github@xwalfie.dev>
2026-07-18 15:40:29 +0200

    feat: refresh mesh and colorset dropdowns after importing

--- scripts/database/settings/SettingsProfile.cs
+++ scripts/database/settings/SettingsProfile.cs
@@ -1092,6 +1092,8 @@ public partial class SettingsProfile
             [
                 new() { Title = "Import Nightly Meshes", Description = "Imports meshes from the nightly folder", OnPressed = () => {
                     ImportMeshesFromNightly();
+                    SettingsManager.Instance.Settings.NoteMesh.List.Values = getAvailableMeshes();
+                    SettingsMenu.Instance.RefreshList(SettingsManager.Instance.Settings.NoteMesh);
                 }}
             ]
         };
@@ -1106,6 +1108,8 @@ public partial class SettingsProfile
             [
                 new() { Title = "Import Nightly Colorsets", Description = "Imports colorsets from the nightly folder", OnPressed = () => {
                     ImportColorsetsFromNightly();
+                    SettingsManager.Load();
+                    SettingsMenu.Instance.RefreshList(SettingsManager.Instance.Settings.NoteColors);
                 }}
             ]
         };
