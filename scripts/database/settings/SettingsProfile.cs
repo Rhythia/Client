800f2bbeba4c3cdf1a6923bddd3071cb9939d7c1
noobour <noobisnotbad1@gmail.com>
2026-08-02 23:03:47 -0400

    Feat: Conspiracy space
    Not 1:1 to sound space

--- scripts/database/settings/SettingsProfile.cs
+++ scripts/database/settings/SettingsProfile.cs
@@ -110,9 +110,9 @@ public partial class SettingsProfile
     public SettingsItem<string> MenuSpace { get; private set; }
 
     /// <summary>
-    /// Overrides the skin's background space for the game
-    /// </summary>
-    [Order]
+	/// Overrides the skin's background space for the game
+	/// </summary>
+	[Order]
     public SettingsItem<string> GameSpace { get; private set; }
 
     /// <summary>
@@ -146,9 +146,9 @@ public partial class SettingsProfile
     public SettingsItem<double> NoteOpacityExponent { get; private set; }
 
     /// <summary>
-    /// Overrides the skin's note mesh
-    /// </summary>
-    [Order]
+	/// Overrides the skin's note mesh
+	/// </summary>
+	[Order]
     public SettingsItem<string> NoteMesh { get; private set; }
 
     /// <summary>
@@ -400,9 +400,9 @@ public partial class SettingsProfile
 
     [Order]
     /// <summary>
-    /// Restarts settings to the game's defaults
-    /// </summary>
-    public SettingsItem<Variant> ResetToDefaults { get; private set; }
+	/// Restarts settings to the game's defaults
+	/// </summary>
+	public SettingsItem<Variant> ResetToDefaults { get; private set; }
 
     #endregion
 
@@ -614,7 +614,7 @@ public partial class SettingsProfile
             UpdateAction = (_, init) => { if (!init) { SkinManager.Load(); } },
             List = new("skin")
             {
-                Values = ["skin", "void", "grid", "squircles", "waves", "galaxy", "tunnel", "tritunnel", "vortex", "solid", "relic"]
+                Values = ["skin", "void", "grid", "squircles", "waves", "galaxy", "tunnel", "tritunnel", "vortex", "solid", "relic", "conspiracy"]
             }
         };
 
@@ -627,7 +627,7 @@ public partial class SettingsProfile
             UpdateAction = (_, init) => { if (!init) { SkinManager.Load(); } },
             List = new("skin")
             {
-                Values = ["skin", "void", "grid", "squircles", "waves", "galaxy", "tunnel", "tritunnel", "vortex", "solid", "relic"]
+                Values = ["skin", "void", "grid", "squircles", "waves", "galaxy", "tunnel", "tritunnel", "vortex", "solid", "relic", "conspiracy"]
             }
         };
 
