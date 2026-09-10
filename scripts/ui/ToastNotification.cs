3b64ebc5d93960ded66ca5ea3bc64f85138f8fef
oldcrate <139006122+oldcrate@users.noreply.github.com>
2026-09-10 14:05:02 +0200

    fix: fixed notification toast wrapping

--- scripts/ui/ToastNotification.cs
+++ scripts/ui/ToastNotification.cs
@@ -22,9 +22,10 @@ public partial class ToastNotification : Node
             return;
         }
 
-        ColorRect notification = template.Instantiate<ColorRect>();
+        PanelContainer notification = template.Instantiate<PanelContainer>();
         SceneManager.Scene.AddChild(notification);
         Color color = new();
+        notification.Visible = true;
 
         switch (Math.Clamp(severity, 0, 2))
         {
@@ -39,9 +40,9 @@ public partial class ToastNotification : Node
                 break;
         }
 
-        notification.GetNode<Label>("Label").Text = message;
-        notification.GetNode<ColorRect>("Severity").Color = color;
-        notification.Visible = true;
+        notification.GetNode<Label>("HBoxContainer/Label").Text = message;
+        notification.GetNode<ColorRect>("HBoxContainer/Severity").Color = color;
+        notification.ResetSize();
         notification.Position += Vector2.Up * activeNotifications * (notification.Size.Y + 8);
 
         Tween inTween = notification.CreateTween();
