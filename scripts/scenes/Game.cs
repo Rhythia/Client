41874d670cf0a2a5309a8de052df2e84e411c079
noobour <noobisnotbad1@gmail.com>
2026-09-11 09:25:24 -0400

    fix: Fade Out keybind resetting to 100, feat: visibility assist, New default colors (#139)
    * fix: Fade Out keybind resetting to 100, New default colors, Squircle space changes
    
    It stores the user's fade out value before resetting and uses that to set it back, added a bunch of new colors to help users customize their game right away
    
    * update notefade script
    
    fg
    
    * feat: visibility assist
    
    a dark ring to assist with visibility on spaces

--- scripts/scenes/Game.cs
+++ scripts/scenes/Game.cs
@@ -15,6 +15,7 @@ public partial class Game : BaseScene
     public static Game Instance;
     public static Attempt Attempt;
     public static bool StartQueued = false;
+    private static double fadeoutToggleValue = -1;
 
     public override void _Ready()
     {
@@ -117,8 +118,23 @@ public partial class Game : BaseScene
         // {
 
         // };
+        PlayerInputController.OnToggleFade += () =>
+        {
+            double val;
 
-        PlayerInputController.OnToggleFade += () => Attempt.Settings.FadeOut.Value = Attempt.Settings.FadeOut > 0 ? 0 : 100;
+            if (Attempt.Settings.FadeOut.Value > 0)
+            {
+                fadeoutToggleValue = Attempt.Settings.FadeOut.Value;
+                val = 0;
+            }
+            else
+            {
+                val = fadeoutToggleValue == -1 ? 100 : fadeoutToggleValue;
+            }
+
+            Attempt.Settings.FadeOut.Value = val;
+        };
+
         PlayerInputController.OnTogglePushback += () => Attempt.Settings.Pushback.Value = !Attempt.Settings.Pushback;
         PlayerInputController.OnRestartPressed += Restart;
     }
