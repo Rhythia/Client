9d291f7a410e55467eb0a4390cdbe4c83682c843
nyarw <github@nyarw.moe>
2026-09-22 10:41:09 +0200

    chore: format VersionLabel.cs

--- scripts/ui/menu/VersionLabel.cs
+++ scripts/ui/menu/VersionLabel.cs
@@ -3,10 +3,10 @@ using Godot;
 
 public partial class VersionLabel : RichTextLabel
 {
-	public override void _Ready()
-	{
+    public override void _Ready()
+    {
         string version = (string)ProjectSettings.GetSetting("application/config/version");
 
         Text = string.Format(CultureInfo.CurrentCulture, Text, version);
-	}
+    }
 }
